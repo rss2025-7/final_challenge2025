aad8fe966212f56df7bce420c7355dc1b0615b97
audrey16 <audrey16@mit.edu>
2025-05-10 02:15:49 -0400

    Backup controller

--- heist_controller/heist_controller/backup.py
+++ heist_controller/heist_controller/backup.py
@@ -3,8 +3,21 @@ import numpy as np
 import rclpy
 import time
 from rclpy.node import Node
+from heist_msgs.msg import HeistState
 from std_msgs.msg import Int32
 from ackermann_msgs.msg import AckermannDriveStamped
+from enum import Enum
+
+class State(Enum):
+    FOLLOW = 1
+    PARK = 2
+    CORRECT = 3
+    END = 4
+
+class Obj(Enum):
+    BANANA_A = 1
+    BANANA_B = 2
+    HOME = 3
 
 class BackupController(Node):
 
@@ -12,20 +25,39 @@ class BackupController(Node):
         super().__init__("backup_controller")
         # Declare parameters to make them available for use
         self.declare_parameter("drive_topic", "default")
-        self.declare_parameter("velocity", -1)
+        self.declare_parameter("velocity", -0.5)
 
         # Fetch constants from the ROS parameter server
         # This is necessary for the tests to be able to test varying parameters!
         self.DRIVE_TOPIC = self.get_parameter('drive_topic').get_parameter_value().string_value
         self.VELOCITY = self.get_parameter('velocity').get_parameter_value().double_value
-        
-        self.timer = self.create_timer(5, self.on_timer)
+        self.state_pub = self.create_publisher(Int32, "/change_info", 1)
+        self.state_sub = self.create_subscription(HeistState, "/heist_state", self.state_callback, 1)
+
+        self.start_time = 0
+        self.started = False
+        self.timer = self.create_timer(0.05, self.on_timer)
 
         self.drive_pub = self.create_publisher(
             AckermannDriveStamped, 
             self.DRIVE_TOPIC,
             10)
-        self.STOPPED = 0
+
+        self.drive_msg = AckermannDriveStamped()
+        self.drive_msg.header.stamp = self.get_clock().now().to_msg()
+        self.drive_msg.header.frame_id = "base_link"
+
+        self.drive_msg.drive.steering_angle = 0.0
+        self.drive_msg.drive.steering_angle_velocity = 0.0 # change as quick as possible
+        self.drive_msg.drive.speed = self.VELOCITY # m/s
+        self.drive_msg.drive.acceleration = 0.0 # change as quick as possible
+        self.drive_msg.drive.jerk = 0.0 # change as quick as possible        
+
+    def state_callback(self, statemsg):
+        if statemsg.state == State.CORRECT.value:
+            if not self.started:
+                self.started = True
+                self.start_time = time.time()                
 
     def _on_delay(self):
         self.timer.cancel()
@@ -33,17 +65,13 @@ class BackupController(Node):
         self.on_timer()
 
     def on_timer(self):
-        drive_msg = AckermannDriveStamped()
-        drive_msg.header.stamp = self.get_clock().now().to_msg()
-        drive_msg.header.frame_id = "base_link"
-
-        drive_msg.drive.steering_angle = 0.0
-        drive_msg.drive.steering_angle_velocity = 0.0 # change as quick as possible
-        drive_msg.drive.speed = self.VELOCITY # m/s
-        drive_msg.drive.acceleration = 0.0 # change as quick as possible
-        drive_msg.drive.jerk = 0.0 # change as quick as possible        
-
-        self.drive_pub.publish(drive_msg)
+        if self.started:
+            self.drive_pub.publish(self.drive_msg)
+            if time.time() - self.start_time > 2:
+                msg = Int32()
+                msg.data = State.FOLLOW.value
+                self.state_pub.publish(msg)
+                self.started = False
 
 def main():
     rclpy.init()
